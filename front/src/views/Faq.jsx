55de85eb935e6c8b2c9efd03da89f2eb451f5449
Jeasika López <33463615+jeasika@users.noreply.github.com>
2022-05-02 16:35:51 -0500

    Update FAQ

--- front/src/views/Faq.jsx
+++ front/src/views/Faq.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 
 function Faq() {
     return (
-      <div className="Faq">
-        <h2 id="heading-faq"><span id="faq"></span>FAQ</h2>
+      <div className="Faq" 
+         style={{
+             margin: '20px'
+         }}
+      >
+        <center><h2 id="heading-faq"><span id="faq"></span>FAQ</h2></center>
 
         <h3 id="Faq1"><span id="FAQ1"></span>¿Cómo reportar un objeto?</h3>
         <p>En la pestaña de objetos, se te pedira que llenes un formulario en el cual llenaras información básica de caracteristicas del objeto, donde fue encontrado, etc.</p>
         <p>Se te pide que al llenar el formulario entregues el objeto a locatec.</p>
         
-        <h3 id="Faq2"><span id="FAQ2"></span>¿Donde o cómo puedo recuperar un objeto?</h3>
+        <h3 id="Faq2"><span id="FAQ2"></span>¿Dónde o cómo puedo recuperar un objeto?</h3>
         <p>Todos los objetos en la pestaña de Objetos Encontrados, fueron entregados a Locatec (Ubicado en aulas I, primer piso).</p>
 
         <h3 id="Faq3"><span id="FAQ3"></span>¿Qué pasa si no encuentro mi Objeto en OBJETOS PERDIDOS?</h3>
