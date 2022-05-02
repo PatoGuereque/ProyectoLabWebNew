85c86975e9dd1fc80366343ebf8e2e54d2919ad2
Pamela Lozano <pamelalozano16@gmail.com>
2022-05-02 14:23:40 -0500

    Error handling y form design

--- front/src/views/ReportarObjeto.jsx
+++ front/src/views/ReportarObjeto.jsx
@@ -1,16 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import Box from '@mui/material/Box';
-import Input from '@mui/material/Input';
+import React, { useState, useRef, useEffect } from 'react';
 import Button from '@mui/material/Button';
-import Alert from '@mui/material/Alert';
 import { useObjectContext } from '../context/objects-context';
+import Alert from '@mui/material/Alert';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
 
 const ReportarObjeto = () => {
   const { postObject, clearPostResponse, postResponse } = useObjectContext();
+  const [showErrorAlert, editErrorAlert] = useState(false);
   const [form, updateForm] = useState({});
+  const hiddenFileInput = useRef(null);
+  const [fileName, setFileName] = useState('');
+  const [errorLabel, setErrorLabel] = useState('Error');
 
   const readFile = (event) => {
+    console.log(event.target);
     if (event.target.files && event.target.files[0]) {
+      setFileName(event.target.files[0].name);
       var reader = new FileReader();
       reader.readAsDataURL(event.target.files[0]);
       reader.onload = function () {
@@ -30,6 +39,24 @@ const ReportarObjeto = () => {
   }, []);
 
   const sendForm = () => {
+    //Error Handling
+    if (
+      !form.campus ||
+      !form.location ||
+      !form.category ||
+      !form.imageBase64 ||
+      form.campus.trim() === '' ||
+      form.location.trim() === '' ||
+      form.category.trim() === '' ||
+      form.imageBase64.trim() === ''
+    ) {
+      setErrorLabel('Debes de llenar todos los campos.');
+      editErrorAlert(true);
+      const timer = setTimeout(() => {
+        editErrorAlert(false);
+      }, 3000);
+      return () => clearTimeout(timer);
+    }
     postObject({
       ...form,
       dateFound: new Date(),
@@ -49,42 +76,96 @@ const ReportarObjeto = () => {
           {postResponse.success ? 'El objeto ha sido reportado!' : 'Error'}
         </Alert>
       )}
-      <Box
-        component="form"
-        sx={{
-          '& > :not(style)': { m: 1 },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <Input
-          placeholder="Campus"
-          name="campus"
-          value={form.campus}
-          onChange={(e) => updateForm({ ...form, campus: e.target.value })}
-        />
-        <Input
-          placeholder="Location"
-          name="location"
-          value={form.location}
-          onChange={(e) => updateForm({ ...form, location: e.target.value })}
-        />
-        <Input
-          placeholder="Category"
-          name="category"
-          value={form.category}
-          onChange={(e) => updateForm({ ...form, category: e.target.value })}
-        />
-        <Input
-          placeholder="Imagen"
-          type="file"
-          name="imageBase64"
-          onChange={(e) => readFile(e)}
-        />
-        <Button size="small" onClick={() => sendForm()}>
-          SUBIR
-        </Button>
-      </Box>
+      {showErrorAlert && (
+        <Alert variant="filled" severity="error" style={{ margin: 20 }}>
+          {errorLabel}
+        </Alert>
+      )}
+      <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
+        <Paper
+          variant="outlined"
+          sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
+        >
+          <Typography component="h1" variant="h4" align="center">
+            Reportar
+          </Typography>
+          <React.Fragment>
+            <Typography variant="h6" gutterBottom>
+              Detalles
+            </Typography>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  id="campus"
+                  label="Campus"
+                  fullWidth
+                  name="campus"
+                  variant="standard"
+                  value={form.campus}
+                  onChange={(e) =>
+                    updateForm({ ...form, campus: e.target.value })
+                  }
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  id="location"
+                  label="Ubicación"
+                  fullWidth
+                  name="location"
+                  variant="standard"
+                  value={form.location}
+                  onChange={(e) =>
+                    updateForm({ ...form, location: e.target.value })
+                  }
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  id="category"
+                  name="category"
+                  label="Categoría"
+                  fullWidth
+                  variant="standard"
+                  value={form.category}
+                  onChange={(e) =>
+                    updateForm({ ...form, category: e.target.value })
+                  }
+                />
+              </Grid>
+              <Grid item xs={12} style={{ textAlign: 'left' }}>
+                <input
+                  placeholder="Imagen"
+                  type="file"
+                  name="imageBase64"
+                  ref={hiddenFileInput}
+                  onChange={(e) => readFile(e)}
+                  style={{ display: 'none' }}
+                />
+                <p>{fileName}</p>
+                <Button
+                  variant="outlined"
+                  onClick={() => hiddenFileInput.current.click()}
+                >
+                  SUBIR IMAGEN
+                </Button>
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  variant="contained"
+                  color="success"
+                  onClick={() => sendForm()}
+                >
+                  REPORTAR
+                </Button>
+              </Grid>
+            </Grid>
+          </React.Fragment>
+        </Paper>
+      </Container>
     </>
   );
 };
