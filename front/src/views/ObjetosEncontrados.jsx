554373d335bfb9882055a6f3ca31a70c27bc7df2
Pamela Lozano <pamelalozano16@gmail.com>
2022-05-01 23:10:19 -0500

    Pantalla de objetos perdidos y reportar objetos

--- front/src/views/ObjetosEncontrados.jsx
+++ front/src/views/ObjetosEncontrados.jsx
@@ -1,5 +1,91 @@
-import React from 'react';
+import React, { useReducer, useEffect, useState } from 'react';
+import {
+  fetchObjects,
+  reducer,
+  deactivateObject,
+} from '../reducers/found-objects-reducer';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import { useAuthContext } from '../context/auth-context';
+import Alert from '@mui/material/Alert';
 
-const ObjetosEncontrados = () => <>Objetos Encontrados</>;
+const ObjetosEncontrados = () => {
+  const [state, dispatch] = useReducer(reducer, { objects: undefined });
+  const { user } = useAuthContext();
+  const [showAlert, editAlert] = useState(false);
+
+  useEffect(() => {
+    fetchObjects(dispatch);
+    console.log(user);
+  }, []);
+
+  console.log(state.objects);
+  const objects = state.objects
+    ? state.objects.map((object) => {
+        return (
+          <Card variant="outlined" sx={{ maxWidth: 345 }} key={object.id}>
+            <CardMedia component="img" height="140" src={object.imageBase64} />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {object.category}
+              </Typography>
+              <Typography gutterBottom variant="h6" component="div">
+                {object.campus}
+              </Typography>
+              <Typography variant="body" color="text.secondary">
+                Fecha de encuentro: {new Date(object.dateFound).toDateString()}
+                <br />
+                Ubicación: {object.location}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button
+                size="small"
+                onClick={() =>
+                  user
+                    ? deactivateObject(
+                        {
+                          id: object._id,
+                          matricula: user.email,
+                        },
+                        dispatch
+                      )
+                    : editAlert(true)
+                }
+              >
+                REPORTAR
+              </Button>
+            </CardActions>
+          </Card>
+        );
+      })
+    : [];
+
+  return (
+    <>
+      <div style={{ margin: 30 }}>
+        {showAlert ? (
+          <Alert severity="error" style={{ margin: 20 }}>
+            Tienes que iniciar sesión para reportar un objeto
+          </Alert>
+        ) : null}
+        <Grid container spacing={2}>
+          {objects
+            ? objects.map((object) => (
+                <Grid item xs={5} md={3}>
+                  {object}
+                </Grid>
+              ))
+            : []}
+        </Grid>
+      </div>
+    </>
+  );
+};
 
 export default ObjetosEncontrados;
--- front/src/views/ReportarObjeto.jsx
+++ front/src/views/ReportarObjeto.jsx
@@ -1,5 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import Input from '@mui/material/Input';
+import Button from '@mui/material/Button';
+import { postObjects } from '../reducers/found-objects-reducer';
+import { useAuthContext } from '../context/auth-context';
+import Alert from '@mui/material/Alert';
 
-const ReportarObjeto = () => <>Reportar Objeto</>;
+const ReportarObjeto = () => {
+  const { user } = useAuthContext();
+  const [showAlert, editAlert] = useState(false);
+  const [form, updateForm] = useState({});
+
+  const readFile = (event) => {
+    console.log(event);
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0]);
+      reader.onload = function () {
+        updateForm({
+          ...form,
+          imageBase64: reader.result,
+        });
+      };
+    }
+  };
+
+  const sendForm = () => {
+    postObjects({
+      ...form,
+      reportingUser: user.email,
+      dateFound: new Date(),
+      status: 'active',
+    });
+    updateForm({});
+    editAlert(true);
+  };
+
+  return (
+    <>
+      {showAlert ? (
+        <Alert variant="filled" severity="success" style={{ margin: 20 }}>
+          El objeto ha sido reportado!
+        </Alert>
+      ) : null}
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1 },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <Input
+          placeholder="Campus"
+          name="campus"
+          value={form.campus}
+          onChange={(e) => updateForm({ ...form, campus: e.target.value })}
+        />
+        <Input
+          placeholder="Location"
+          name="location"
+          value={form.location}
+          onChange={(e) => updateForm({ ...form, location: e.target.value })}
+        />
+        <Input
+          placeholder="Category"
+          name="category"
+          value={form.category}
+          onChange={(e) => updateForm({ ...form, category: e.target.value })}
+        />
+        <Input
+          placeholder="Imagen"
+          type="file"
+          name="imageBase64"
+          onChange={(e) => readFile(e)}
+        />
+        <Button size="small" onClick={() => sendForm()}>
+          SUBIR
+        </Button>
+      </Box>
+    </>
+  );
+};
 
 export default ReportarObjeto;
