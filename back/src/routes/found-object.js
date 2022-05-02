4c98c7c5badb47ce96568a92988e199106b23484
Domene99 <jadomene99@gmail.com>
2022-05-01 20:51:52 -0500

    Add object routes

--- back/src/routes/found-object.js
+++ back/src/routes/found-object.js
@@ -1,11 +1,21 @@
 import express from 'express';
+import { foundObjectRepository } from '../database';
 
 const foundObject = express();
 
-foundObject.get('/found-objects', (_req, res) => {
+foundObject.get('/found-objects', async (_req, res) => {
+  const objects = await foundObjectRepository.getFoundObjects();
+  const activeObjects = objects.filter((object) => object.status === 'active');
+
   res.status(200).json({
-    pong: new Date().getTime(),
+    objects: activeObjects,
   });
+});
+
+foundObject.post('/report-objects', async (_req, res) => {
+  // Posts
+  foundObjectRepository.createFoundObject(_req.body);
+  res.status(200).json(_req.body);
 });
 
 export default foundObject;
