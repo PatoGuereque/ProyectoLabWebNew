2d62a7071a9dc023c85e62a8437469996337378b
Jeasika López <33463615+jeasika@users.noreply.github.com>
2022-05-02 16:31:36 -0500

    Update Home

--- front/src/views/Home.jsx
+++ front/src/views/Home.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
 
-const Home = () => <>Home</>;
+function Home() {
+     return (
+        <div
+            style={{
+                width: "100%",
+                height: "500px",
+                background: `url('${process.env.PUBLIC_URL}/boxes.jpg')`,
+                backgroundPosition: 'center',
+            }}
+            >
+            <center>
+                <img src={process.env.PUBLIC_URL+"logolocatec.png"} alt = "Logo Locatec" />
+            </center>
+        </div>
+     );
+ }
 
 export default Home;
