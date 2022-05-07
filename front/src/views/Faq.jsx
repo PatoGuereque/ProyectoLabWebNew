0170efcd318e255e05ae7b0362be5bd8ffb2efcd
Jeasika López <33463615+jeasika@users.noreply.github.com>
2022-05-07 18:45:49 -0500

    Update FAQ

--- front/src/views/Faq.jsx
+++ front/src/views/Faq.jsx
@@ -1,26 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Data } from '../components/DataFAQ';
+import styled from 'styled-components';
+import Typography from '@mui/material/Typography';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import RemoveIcon from '@mui/icons-material/Remove';
 
-function Faq() {
-    return (
-      <div className="Faq" 
-         style={{
-             margin: '20px'
-         }}
-      >
-        <center><h2 id="heading-faq"><span id="faq"></span>FAQ</h2></center>
+const AccordionSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  height: 100vh;
+  background: #fff;
+`;
 
-        <h3 id="Faq1"><span id="FAQ1"></span>¿Cómo reportar un objeto?</h3>
-        <p>En la pestaña de objetos, se te pedira que llenes un formulario en el cual llenaras información básica de caracteristicas del objeto, donde fue encontrado, etc.</p>
-        <p>Se te pide que al llenar el formulario entregues el objeto a locatec.</p>
-        
-        <h3 id="Faq2"><span id="FAQ2"></span>¿Dónde o cómo puedo recuperar un objeto?</h3>
-        <p>Todos los objetos en la pestaña de Objetos Encontrados, fueron entregados a Locatec (Ubicado en aulas I, primer piso).</p>
+const Container = styled.div`
+  position: absolute;
+  top: 10%;
+  box-shadow: 2px 10px 35px 1px rgba(153, 153, 153, 0.3);
+`;
 
-        <h3 id="Faq3"><span id="FAQ3"></span>¿Qué pasa si no encuentro mi Objeto en OBJETOS PERDIDOS?</h3>
-        <p>No todos los objetos que se encuentran en locatec estan en la pagina por lo que te recomendamos darte una vuelta a locatec (Ubicado en aulas I, primer piso).</p>
-
-      </div>
-    );
+const Wrap = styled.div`
+  background: #0474A9;
+  color: #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  text-align: center;
+  cursor: pointer;
+  h1 {
+    padding: 2rem;
+    font-size: 1rem;
   }
+  span {
+    margin-right: 1.5rem;
+  }
+`;
 
-export default Faq;
+const Dropdown = styled.div`
+  background: #11A1B7;
+  color: #fff;
+  width: 100%;
+  height: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 1px solid #00ffb9;
+  border-top: 1px solid #00ffb9;
+  p {
+    font-size: 1rem;
+  }
+  margin-right: 1rem; 
+`;
+
+const Accordion = () => {
+  const [clicked, setClicked] = useState(false);
+  const toggle = index => {
+    if (clicked === index) {
+      //if clicked question is already active, then close it
+      return setClicked(null);
+    }
+
+    setClicked(index);
+  };
+  
+  return (
+      <AccordionSection>
+        <Container>
+          <Typography component="h1" variant="h4" align="center" color = "#0474A9">
+            FAQ
+          </Typography>
+          {Data.map((item, index) => {
+            return (
+              <>
+                <Wrap onClick={() => toggle(index)} key={index}>
+                  <h1>{item.question}</h1>
+                  <span>{clicked === index ? <RemoveIcon /> : <AddBoxIcon />}</span>
+                </Wrap>
+                {clicked === index ? (
+                  <Dropdown>
+                    <p>{item.answer}</p>
+                  </Dropdown>
+                ) : null}
+              </>
+            );
+          })}
+        </Container>
+      </AccordionSection>
+  );
+};
+
+export default Accordion;
