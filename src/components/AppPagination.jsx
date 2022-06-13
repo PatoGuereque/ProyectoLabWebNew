2b12273c05f8c8ffd50e6f667c947f4223c48121
Juan Briones <JuanMBriones@outlook.com>
2022-06-13 13:34:48 -0500

    Resolve Deprecated material ui module

--- src/components/AppPagination.jsx
+++ src/components/AppPagination.jsx
@@ -1,35 +1,30 @@
 import React from 'react';
 import { Pagination } from '@mui/material';
-import { makeStyles } from '@mui/styles';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    position: 'fixed',
-    bottom: 0,
-    zIndex: 200,
-    padding: '10px 80px',
-
-    color: 'black',
-    width: '100%',
-  },
-  container: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-}));
 
 const AppPagination = ({ setPage, pageNumber }) => {
-  const classes = useStyles();
-
   const handleChange = (page) => {
     setPage(page);
     window.scroll(0, 0);
   };
 
   return (
-    <div className={classes.container}>
-      <div className={classes.root}>
+    <div
+      sx={{
+        position: 'fixed',
+        bottom: 0,
+        zIndex: 200,
+        padding: '10px 80px',
+        color: 'black',
+        width: '100%',
+      }}
+    >
+      <div
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
         <Pagination
           onChange={(e) => handleChange(e.target.textContent)}
           color="primary"
