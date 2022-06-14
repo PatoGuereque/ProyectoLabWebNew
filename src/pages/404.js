14969b315eb9b3f9c12d62f8b8398fc8d0c310d2
Juan Carlos Gómez A <49078525+Juancgmz@users.noreply.github.com>
2022-06-14 12:40:41 -0500

    Class->ClassName

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Custom404() {
   return (
     <div id="notfound">
-		<div class="notfound">
-			<div class="notfound-404">
+		<div className="notfound">
+			<div className="notfound-404">
 				<h3>¡Oops! Parece que esta página se perdió...</h3>
 				<h1><span>4</span><span>0</span><span>4</span></h1>
 			</div>
