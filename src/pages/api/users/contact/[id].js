71b79a5ac7381bcfd5969677725a8839ab42ec6d
Juan Briones <JuanMBriones@outlook.com>
2022-06-17 15:40:09 -0500

    Feature: Add Reporting information at Reporting table

---
+++ src/pages/api/users/contact/[id].js
@@ -0,0 +1,36 @@
+import { authRequired } from '../../../../middlewares/auth-required';
+import prisma from '../../../../lib/prisma';
+
+const handler = async (req, res) => {
+  const { id } = req.query;
+
+  if(!session || (session && !req.session.user.roles.includes('admin'))) {
+    return res.status(403).json({
+      message: 'You are not authorized to perform this action',
+    });  
+  }
+
+  if (isNaN(id)) {
+    return res.status(400).json({
+      message: 'Invalid id parameter',
+    });
+  }
+
+  const object = await prisma.user.findFirst({
+    where: {
+      id: parseInt(id),
+    },
+  });
+
+  if (!object) {
+    return res.status(404).json({
+      message: `${id} not found in database`,
+    });
+  }
+
+  return res.status(200).json({
+    message: 'Reclaim successful',
+  });
+};
+
+export default authRequired(handler);
--- src/pages/reportes.jsx
+++ src/pages/reportes.jsx
@@ -5,11 +5,22 @@ import { useUsersContext } from '../context/users-context';
 import { useObjectContext } from '../context/objects-context';
 import { DataGrid } from '@mui/x-data-grid';
 import { useSession } from 'next-auth/react';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
 
 const Reports = () => {
   const { data: session } = useSession();
   const { objects } = useObjectContext();
   console.log(objects);
+  const [modalObject, setModalObject] = useState(undefined);
+  const handleModalClose = () => setModalObject(undefined);
 
   if (!session || (session && session.user.roles != 'admin')) {
     return (
@@ -25,8 +36,12 @@ const Reports = () => {
         id: object.id,
         category_name: object.category.name,
         location_name: object.location.name,
-        user: object.reportingUserId,
+        user_reported: object.reportingUserId,
+        user_claimed: object.claimedById,
         status: object.status,
+        image: object.image,
+        comments: object.comments,
+        campus: object.location.campus.name,
       };
     });
   };
@@ -44,18 +59,58 @@ const Reports = () => {
 
   //onsole.log(objects);
   return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={formatObjects(objects)}
-        columns={columns}
-        pageSize={5}
-        onCellClick={(e) => {
-          console.log(e);
-        }}
-        rowsPerPageOptions={[5]}
-        checkboxSelection
-      />
-    </div>
+    <>
+      <Dialog
+        open={modalObject !== undefined}
+        onClose={handleModalClose}
+        maxWidth="md"
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <DialogTitle>{modalObject?.row.category_name}</DialogTitle>
+        <Box
+          sx={{
+            px: 2,
+          }}
+        >
+          <CardMedia
+            component="img"
+            height="250"
+            src={modalObject?.row.image}
+          />
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Localizado en: {modalObject?.row.campus},{' '}
+            {modalObject?.row.location_name}
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Comentarios: {modalObject?.row.comments}
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Creador de publicacion: {modalObject?.row.user_reported}
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Dueno del objeto: {modalObject?.row.user_claimed}
+          </Typography>
+        </Box>
+
+        <DialogActions>
+          <Button onClick={handleModalClose}>Cerrar</Button>
+        </DialogActions>
+      </Dialog>
+      <div style={{ height: 400, width: '100%' }}>
+        <DataGrid
+          rows={formatObjects(objects)}
+          columns={columns}
+          pageSize={5}
+          onCellClick={(e, value) => {
+            console.log(e, value);
+            setModalObject(e);
+          }}
+          rowsPerPageOptions={[5]}
+          checkboxSelection
+        />
+      </div>
+    </>
   );
 };
 
